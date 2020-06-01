68040522f495460b3593290ec4dab82065edd762
lucasmartiness <lucas.alcantarilla97@gmail.com>
2020-06-01 18:17:16 -0300

    .

--- frontend/.env.js
+++ frontend/.env.js
@@ -1,6 +1,6 @@
 export default {
-  APP_NAME: 'Biblioteca',
-  SERVER_PORT: 3000,
-  SERVER_HOST: 'http://localhost',
-  SERVER_HOST_FULL: 'http://localhost:3000'
-} 
+    APP_NAME: 'Biblioteca',
+    SERVER_PORT: 3000,
+    SERVER_HOST: process.env.HOST || 'http://localhost',
+    SERVER_HOST_FULL: process.env.HOST_PORT || 'http://localhost:3000'
+}
