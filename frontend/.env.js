274c4a504d4bb2796f48f7f385f1130c0bdc010e
lucasmartiness <lucas.alcantarilla97@gmail.com>
2020-06-01 18:43:16 -0300

    .

--- frontend/.env.js
+++ frontend/.env.js
@@ -1,8 +1,8 @@
-export default env = {
+export default {
     APP_NAME: 'Biblioteca',
     SERVER_PORT: 3000,
     // SERVER_HOST: 'http://localhost',
     // SERVER_HOST_FULL: 'http://localhost:3000'
-    SERVER_HOST: 'https://library-app-simple.herokuapp.com/',
-    SERVER_HOST_FULL: 'https://library-app-simple.herokuapp.com/'
+    SERVER_HOST: 'https://library-app-simple.herokuapp.com',
+    SERVER_HOST_FULL: 'https://library-app-simple.herokuapp.com'
 }
