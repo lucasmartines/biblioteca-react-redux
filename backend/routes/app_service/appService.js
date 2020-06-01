cc18c13f772c6af04ef86da7235896f29fc2ff16
lucasmartiness <lucas.alcantarilla97@gmail.com>
2020-06-01 18:12:58 -0300

    .

--- backend/routes/app_service/appService.js
+++ backend/routes/app_service/appService.js
@@ -12,8 +12,11 @@ module.exports = (app) => {
     app.use(bodyParser.json())
 
     // static files from front end
-    app.use(express.static(__dirname + './../../../frontend/public'))
-        //    app.use(express.static(base + './../frontend/public'))
+
+    app.use(express.static(path.resolve(__dirname, '../../public')))
+    console.log(path.resolve(__dirname, '../../public'))
+
+    //    app.use(express.static(base + './../frontend/public'))
 
     /** send static file front end */
     app.get('/', (req, res) => {
